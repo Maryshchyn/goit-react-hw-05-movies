96405c079699f8d3fab7e5ff56e2ec6d4980b90e
Maryshchyn <maryshchyn89@gmail.com>
2022-10-25 11:55:04 +0300

    ll

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 import { AppBarLink, AppBarUl, AppBarLi } from './AppBar.styled';
 
 export const AppBar = () => {
