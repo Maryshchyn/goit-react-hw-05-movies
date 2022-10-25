501640b28aae8461a6bfe3737a2e42f2469abc2b
Maryshchyn <maryshchyn89@gmail.com>
2022-10-25 11:30:29 +0300

    qwe

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -7,7 +7,7 @@ export const Cast =()=> {
   const [cast, setCast] = useState([]);
     const { movieId } = useParams();
     const KEY = '518d624082d6ba170a75ad4d399f89a3';
-    console.log(cast)
+    
 
   useEffect(() => {
      axios
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -3,12 +3,17 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 
 export const Reviews = () => {
-    const [reviews, setReviews] = useState([]);
-    console.log(reviews)
-    const KEY = '518d624082d6ba170a75ad4d399f89a3';
-    const { movieId } = useParams();
-    useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${KEY}&language=en-US&page=1`).then(res => { setReviews(res.data.results) })
+  const [reviews, setReviews] = useState([]);
+  const { movieId } = useParams();
+  const KEY = '518d624082d6ba170a75ad4d399f89a3';
+    
+  
+  useEffect(() => {
+    axios
+      .get(`https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${KEY}&language=en-US&page=1`)
+      .then(res => {
+        return setReviews(res.data.results);
+      })
     }, [movieId]);
 
     
